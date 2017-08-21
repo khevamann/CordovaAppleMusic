78f4cd2be672ff77621b154b0b184304bb30beae
khevamann <khevamann@gmail.com>
2017-08-20 17:10:24 -0700

    Update and rename appleMusicPlaylists.js to appleMusic.js

--- www/appleMusic.js
+++ www/appleMusic.js
@@ -9,31 +9,31 @@ window.appleMusicPluginSeeked = (function(){})
 window.appleMusicPluginStopped = (function(){})
 module.exports = {
     getCountryCode: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "getCountryCode", []);
+        exec(successCallback, errorCallback, "AppleMusic", "getCountryCode", []);
     },
     getDuration: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "getDuration", []);
+        exec(successCallback, errorCallback, "AppleMusic", "getDuration", []);
     },
     getPosition: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "getPosition", []);
+        exec(successCallback, errorCallback, "AppleMusic", "getPosition", []);
     },
     getStatus: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "getStatus", []);
+        exec(successCallback, errorCallback, "AppleMusic", "getStatus", []);
     },
     init: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "init", []);
+        exec(successCallback, errorCallback, "AppleMusic", "init", []);
     },
     getPlayLists: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "getPlayLists", []);
+        exec(successCallback, errorCallback, "AppleMusic", "getPlayLists", []);
     },
     addSongstoPlayList: function (playListId, songIds, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "addSongstoPlayList", [playListId, songIds]);
+        exec(successCallback, errorCallback, "AppleMusic", "addSongstoPlayList", [playListId, songIds]);
     },
     getSongs: function (playlistId, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "getSongs", [playlistId]);
+        exec(successCallback, errorCallback, "AppleMusic", "getSongs", [playlistId]);
     },
     createPlayList: function (playlistName, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "createPlayList", [playlistName]);
+        exec(successCallback, errorCallback, "AppleMusic", "createPlayList", [playlistName]);
     },
     onPlay: function(f) {
         window.appleMusicPluginPlaying = f
@@ -45,21 +45,21 @@ module.exports = {
         window.appleMusicPluginStopped = f
     },
     pause: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "pause", []);
+        exec(successCallback, errorCallback, "AppleMusic", "pause", []);
     },
     playTrack: function (trackId, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "playTrack", [trackId]);
+        exec(successCallback, errorCallback, "AppleMusic", "playTrack", [trackId]);
     },
     requestAuthorization: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "requestAuthorization", []);
+        exec(successCallback, errorCallback, "AppleMusic", "requestAuthorization", []);
     },
     resume: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "resume", []);
+        exec(successCallback, errorCallback, "AppleMusic", "resume", []);
     },
     seek: function (seconds, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "seek", [seconds]);
+        exec(successCallback, errorCallback, "AppleMusic", "seek", [seconds]);
     },
     stop: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AppleMusicPlaylists", "stop", []);
+        exec(successCallback, errorCallback, "AppleMusic", "stop", []);
     }
 };
